86a5b681636a360c283f88213076477f8d4b0578
Fernando Futila <fernando.futila@gmail.com>
2021-08-24 19:37:12 +0100

    Estrutura backend

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require ('cors');
+const path = require('path');
+
+
+
+const app = express();
+
+const server = require ('http').Server(app);
+const io = require('socket.io')(server,{
+    cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+}
+
+});
+
+
+mongoose.connect('mongodb+srv://semana:week@goweek-backend.0toen.mongodb.net/instagramClone?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+    }
+);
+
+app.use((req, res, next)=>{
+    req.io = io;
+
+    return next();
+});
+
+app.use(cors());
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
+
+
+app.use(require('./routes'));
+
+server.listen(3333);
