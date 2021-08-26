111f4e89717096aa9a54d32e35d769a46bc76be6
Fernando Futila <fernando.futila@gmail.com>
2021-08-27 00:34:59 +0100

    Adiciona url em Post.js e variáveis ambiente

--- src/models/Post.js
+++ src/models/Post.js
@@ -13,6 +13,14 @@ const PostSchema = new mongoose.Schema({
     }
 },{
     timestamps: true,
+    toObject: {virtuals: true},
+    toJSON: {virtuals: true},
+
+});
+
+PostSchema.virtual('url').get(function(){
+    const url = process.env.URL || 'http://localhost:3333'; 
+    return `${url}/files/${encodeURIComponent(this.image)}`;
 });
 
 module.exports = mongoose.model('Post', PostSchema);
